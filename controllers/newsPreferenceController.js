4d00c2a575b7730f490becf73d599f63d9092237
HarshitM2411 <meenaharshit30@gmail.com>
2026-02-28 22:16:25 +0530

    feat(news-aggregator): caching implemented

--- controllers/newsPreferenceController.js
+++ controllers/newsPreferenceController.js
@@ -1,11 +1,23 @@
 const users = require('../models/userModel');
 const axios = require('axios');
 
+const newsCache = new Map();
+const CACHE_TTL_MS = 5 * 60 * 1000;
+
 const getPrefferedNews = async (email) => {
     const body = { email };
     const dbUser = await users.findOne(body);
     if (!dbUser) {
         return { message: 'User not found', news: [] };
+    }
+
+    const cacheKey = dbUser.preferences.join(' AND ');
+    if (newsCache.has(cacheKey)) {
+        const { data, cachedAt } = newsCache.get(cacheKey);
+        if (Date.now() - cachedAt < CACHE_TTL_MS) {
+            return { message: 'News retrieved successfully', news: data };
+        }
+        newsCache.delete(cacheKey);
     }
 
     try {
@@ -14,9 +26,10 @@ const getPrefferedNews = async (email) => {
                 'X-Api-Key': process.env.NEWS_API_KEY
             },
             params: {
-                q: dbUser.preferences.join(),
+                q: cacheKey,
             }
         });
+        newsCache.set(cacheKey, { data: response.data, cachedAt: Date.now() });
         return { message: 'News retrieved successfully', news: response.data };
 
     } catch (error) {
